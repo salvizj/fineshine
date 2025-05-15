583ac0e12db35fa321e08c6a3accaa8b009444be
salvis <sjansons2003@gmail.com>
2025-05-15 23:23:03 +0300

    astro inti

---
+++ src/i18n/ui.ts
@@ -0,0 +1,19 @@
+export const languages = {
+	en: "English",
+	lv: "Latvian",
+	ru: "Russian",
+}
+
+export const defaultLang = "lv"
+
+export const ui = {
+	en: {
+		"nav.home": "Home",
+	},
+	lv: {
+		"nav.home": "Mājas",
+	},
+	ru: {
+		"nav.home": "Дом",
+	},
+} as const
