3e9a6639af90df78cfbaf06c599859d64f7e77fb
salvis <sjansons2003@gmail.com>
2025-05-20 15:28:16 +0300

    fix: tailwind bug fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,14 @@
-// @ts-check
 import { defineConfig } from "astro/config"
-
 import tailwindcss from "@tailwindcss/vite"
 import { languageOptions, defaultLanguageCode } from "./src/i18n/ui"
 
-// https://astro.build/config
 export default defineConfig({
-	vite: {
-		plugins: [tailwindcss()],
-	},
+	vite: { plugins: [tailwindcss()] },
 	i18n: {
 		locales: Object.keys(languageOptions),
 		defaultLocale: defaultLanguageCode,
 		routing: {
-			prefixDefaultLocale: false,
+			prefixDefaultLocale: true,
 		},
 	},
 })
