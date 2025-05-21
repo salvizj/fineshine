e9412fc237ede3aed687397807705fa128d9814e
salvis <sjansons2003@gmail.com>
2025-05-22 00:12:51 +0300

    changes: style changes

--- src/scripts/NavigationMenuToggle.ts
+++ src/scripts/NavigationMenuToggle.ts
@@ -1,42 +1,32 @@
 export default function NavigationMenuToggle() {
-	let navOpenBtn = null
-	let navCollapseBtn: HTMLElement | null = null
-	let navOpen: HTMLElement | null = null
-	let navCollapsed: HTMLElement | null = null
+	document.addEventListener("DOMContentLoaded", () => {
+		const navOpenBtn = document.querySelector<HTMLElement>("#nav-open-btn")
+		const navCollapseBtn =
+			document.querySelector<HTMLElement>("#nav-collapse-btn")
+		const navOpen = document.querySelector<HTMLElement>(".nav-open")
+		const navCollapsed =
+			document.querySelector<HTMLElement>(".nav-collapsed")
 
-	const resetNavIfHidden = () => {
-		if (!navOpenBtn || !navOpen || !navCollapsed) return
+		if (!navOpenBtn || !navCollapseBtn || !navOpen || !navCollapsed) return
 
-		const navOpenBtnIsVisible =
-			window.getComputedStyle(navOpenBtn).display !== "none"
+		navOpenBtn.addEventListener("click", () => {
+			navOpen.classList.remove("hidden")
+			navCollapsed.classList.add("hidden")
+			navOpenBtn.classList.add("hidden")
+		})
 
-		if (!navOpenBtnIsVisible) {
+		navCollapseBtn.addEventListener("click", () => {
 			navOpen.classList.add("hidden")
 			navCollapsed.classList.remove("hidden")
 			navOpenBtn.classList.remove("hidden")
-		}
-	}
+		})
 
-	document.addEventListener("DOMContentLoaded", () => {
-		navOpenBtn = document.getElementById("nav-open-btn")
-		navCollapseBtn = document.getElementById("nav-collapse-btn")
-		navOpen = document.querySelector(".nav-open")
-		navCollapsed = document.querySelector(".nav-collapsed")
-
-		if (navOpenBtn && navCollapseBtn && navOpen && navCollapsed) {
-			navOpenBtn.addEventListener("click", () => {
-				navOpen!.classList.remove("hidden")
-				navCollapsed!.classList.add("hidden")
-				navOpenBtn!.classList.add("hidden")
-			})
-
-			navCollapseBtn.addEventListener("click", () => {
-				navOpen!.classList.add("hidden")
-				navCollapsed!.classList.remove("hidden")
-				navOpenBtn!.classList.remove("hidden")
-			})
-		}
+		window.addEventListener("resize", () => {
+			if (window.getComputedStyle(navOpenBtn).display === "none") {
+				navOpen.classList.add("hidden")
+				navCollapsed.classList.remove("hidden")
+				navOpenBtn.classList.remove("hidden")
+			}
+		})
 	})
-
-	window.addEventListener("resize", resetNavIfHidden)
 }
