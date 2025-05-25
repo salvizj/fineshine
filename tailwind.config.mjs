ebc6d2236a18c5802567892d407cfaea13863ce6
salvis <sjansons2003@gmail.com>
2025-05-25 23:41:41 +0300

    change: tailwind color vairiables

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,6 +6,26 @@ export default {
 			fontFamily: {
 				montserrat: ["Montserrat", "sans-serif"],
 			},
+			colors: {
+				text: {
+					primary: "var(--color-text-primary)",
+					secondary: "var(--color-text-secondary)",
+					tertiary: "var(--color-text-tertiary)",
+				},
+				bg: {
+					default: "var(--color-bg-default)",
+					muted: "var(--color-bg-muted)",
+					card: "var(--color-bg-card)",
+				},
+				border: {
+					default: "var(--color-border-default)",
+					muted: "var(--color-border-muted)",
+				},
+				hover: {
+					primary: "var(--color-hover-primary)",
+					muted: "var(--color-hover-muted)",
+				},
+			},
 		},
 	},
 	plugins: [],
