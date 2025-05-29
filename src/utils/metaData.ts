fca711a6f57824b4afbd9cb24cb4b09f35b40b67
salvis <sjansons2003@gmail.com>
2025-05-29 21:54:55 +0300

    change: metaData updates

--- src/utils/metaData.ts
+++ src/utils/metaData.ts
@@ -10,23 +10,23 @@ const metaData: Record<LanguageCode, LanguageMeta> = {
 	lv: {
 		title: "Fineshine auto detailing studija",
 		description:
-			"Sertificēti auto detailing pakalpojumi Latvijā: ķīmiskā tīrīšana, auto pulēšana, aizsargpārklājumi, lukturu restaurācija, ādas krāsošana un ozonēšana.",
+			"Sertificēta detailera pakalpojumi tepat ārpus Rīgas - Salaspilī — auto ķīmiskā tīrīšana, pulēšana, aizsargpārklājumi, lukturu restaurācija, ādas krāsošana un ozonēšana.",
 		keywords:
-			"auto detailing, auto ķīmiskā tīrīšana, auto pulēšana, aizsargpārklājumi, lukturu restaurācija, ādas krāsošana, ozonēšana, auto salons, detailing Latvija",
+			"auto detailing, auto ķīmiskā tīrīšana, auto pulēšana, aizsargpārklājumi, lukturu restaurācija, ādas krāsošana, ozonēšana, auto salons, detailing tepat ārpus Rīgas Salaspilī.",
 	},
 	en: {
 		title: "Fineshine auto detailing studio",
 		description:
-			"Certified auto detailing services in Latvia: chemical cleaning, polishing, protective coatings, headlight restoration, leather coloring, and ozone treatment.",
+			"Certified detailer services just outside Riga – in Salaspils — car chemical cleaning, polishing, protective coatings, headlight restoration, leather coloring, and ozonation.",
 		keywords:
-			"auto detailing, chemical cleaning, polishing, protective coatings, headlight restoration, leather coloring, ozone treatment, car interior, detailing Latvia",
+			"auto detailing, chemical cleaning, polishing, protective coatings, headlight restoration, leather coloring, ozone treatment, car interior, detailing outside Riga in Salaspils",
 	},
 	ru: {
 		title: "Студия авто детайлинга Fineshine",
 		description:
-			"Сертифицированные услуги авто детайлинга в Латвии: химчистка, полировка, защитные покрытия, восстановление фар, окраска кожи и озонирование.",
+			"Сертифицированные услуги детейлера недалеко от Риги — в Саласпилсе — химчистка автомобиля, полировка, защитные покрытия, реставрация фар, покраска кожи и озонирование.",
 		keywords:
-			"авто детайлинг, химчистка авто, полировка авто, защитные покрытия, восстановление фар, окраска кожи, озонирование, салон авто, детайлинг Латвия",
+			"авто детайлинг, химчистка авто, полировка авто, защитные покрытия, восстановление фар, окраска кожи, озонирование, салон авто, детайлинг, недалеко от Риги  в Саласпилсе",
 	},
 }
 
